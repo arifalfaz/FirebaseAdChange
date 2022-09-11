5f913129c09f9de00b9487c91e14a80f8893b714
Arifalfaz98- <arifalfaz98@gmail.com>
2022-09-11 11:28:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FirebaseAdChange
+ FirebaseAdChange
---
+++ app/src/main/java/com/elecsiya/firebaseadchange/MainActivity.java
@@ -0,0 +1,42 @@
+package com.elecsiya.firebaseadchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ktx.Firebase;
+
+public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+    TextView unit_text;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        AdView adView  = new AdView(this);
+        adView.setAdSize(AdSize.LARGE_BANNER);
+        mAdView.loadAd(adRequest);
+
+
+
+    }
+}
